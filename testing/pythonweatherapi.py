a34d86827106b6922d95cbed85799fadf81a500d
AustinZhang1 <austinzhang0508@gmail.com>
2023-10-04 23:23:41 -0700

    weather api additions

--- testing/pythonweatherapi.py
+++ testing/pythonweatherapi.py
@@ -7,7 +7,7 @@ from datetime import date
 '''today = date.today()
 print(today)
 print(f"today's date: {today}")'''
-response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=32.7157&longitude=-117.1647&hourly=temperature_2m,precipitation_probability,precipitation,rain&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum&temperature_unit=fahrenheit&windspeed_unit=mph&timezone=America%2FLos_Angeles")
+response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m,precipitation_probability,precipitation&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&temperature_unit=fahrenheit&timezone=America%2FLos_Angeles")
 print(response.status_code)
 #print(response.json())
 weather = response.json()
@@ -16,16 +16,21 @@ def printweather(weather):
     counter = 0
     temps = []
     dates = []
+    precipitation = []
     for x in weather["hourly"]["temperature_2m"]:
         temps.append(x)
     for x in weather["daily"]["time"]:
         dates.append(x)
+    for x in weather["hourly"]["precipitation_probability"]:
+        precipitation.append(x)
     print(dates)
     print(temps)
+    print(precipitation)
     for i in range(7):
         print("Date: ",{dates[i]})
         for i in range(24):
             print(f"The temperature at {i} o'clock is {temps[counter]} degrees.")
+            print(f"The chance of precipitation at {i} o'clock is {precipitation[counter]}%\n")
             counter += 1
 
 printweather(weather)
